1ebc64b40a9d90f71cc0352e82ae4eb43b78a2dc
Anthony Tleiji <anthonytleiji@gmail.com>
2023-03-23 10:12:48 +0200

    Added Basic Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./shared/utils/theme";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/Home.page";
+import RegisterPage from "./pages/Register.page";
+import SigninPage from "./pages/Signin.page";
 
 function App() {
-  return <ThemeProvider theme={theme}>Hello World</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/signin" element={<SigninPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
