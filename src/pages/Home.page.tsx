0567d5bdd4b051f42ced9f8d0449e02035c8e467
Anthony Tleiji <anthonytleiji@gmail.com>
2023-04-03 17:10:41 +0300

    Added Product Component

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import HeaderComponent from "../features/products/components/Header.component";
+import { useAppSelector, useAppDispatch } from "../hooks/redux/hooks";
+import ProductComponent from "../features/products/components/Product.component";
 
 const HomePage = () => {
+  const { cart, products } = useAppSelector((state) => state.product);
+  const dispatch = useAppDispatch();
   return (
     <div>
       <HeaderComponent />
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "48px",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        {products.map((product) => (
+          <ProductComponent key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
